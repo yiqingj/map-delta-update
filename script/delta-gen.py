1e1259664ecca4342480e66a84c5b96783641fcb
yiqingj <yiqingj@telenav.com>
2014-02-26 16:09:05 -0800

    init script

---
+++ script/delta-gen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+__author__ = 'yiqingj'
+
+import os
+import sys
+import json
+
+usage = 'delta-gen.py [config-file]'
+
+
+def create_command():
+    if len(sys.argv) != 2:
+        print usage
+        return
+    file_name = open(sys.argv[1], 'r')
+    config = json.load(file_name)
+    count = len(config)
+    command = '../../bin/osmosis --rri workingDirectory=. --simc --tc %d' % count
+    print "Number of area", count
+    for area in config:
+        top_left = area["bound"]["top-left"].split(",")
+        bottom_right = area["bound"]["bottom-right"].split(",")
+        filter_cmd = ' --bounding-box-change top=%s left=%s bottom=%s right=%s --write-replication workingDirectory=%s' \
+                     % (top_left[0], top_left[1], bottom_right[0], bottom_right[1], area['file'])
+        command += filter_cmd
+    print command
+
+if __name__ == "__main__":
+    create_command()
+
+
+
+
